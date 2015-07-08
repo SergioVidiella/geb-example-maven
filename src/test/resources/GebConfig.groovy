7acfc2c7370d45d37d9f9cba0d1dc5b1f96c623d
SergioVidiella <sergio.vidiella@i2cat.net>
2015-07-08 10:01:06 +0200

    Update GebConfig.groovy

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -9,7 +9,7 @@ import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.remote.RemoteWebDriver
 
 driver = {
-            new RemoteWebDriver(new URL("http://192.168.2.122:4444/wd/hub"), DesiredCapabilities.InternetExplorer())
+            new RemoteWebDriver(new URL("http://192.168.2.122:4444/wd/hub"), DesiredCapabilities.internetExplorer())
         }
 
 baseUrl = "http://gebish.org"
